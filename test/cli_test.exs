3cbcd9ba2ea57a6359e0b5eebacc71248336d1c7
Benoit Tigeot <benoit@hopsandfork.com>
2016-05-30 08:14:44 +0200

    Add one integration test with VCR tape

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,10 +1,17 @@
 defmodule CliTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+  import ExUnit.CaptureIO
   doctest Issues
 
-  import Issues.CLI, only: [ parse_args: 1,
+  import Issues.CLI, only: [ main: 1,
+                             parse_args: 1,
                              sort_into_ascending_order: 1,
                              convert_to_list_of_maps: 1 ]
+
+  setup_all do
+    HTTPoison.start
+  end
 
   test ":help returnd by options parsing with -h and --help options" do
     assert parse_args(["-h", "anything"]) == :help
@@ -28,5 +35,21 @@ defmodule CliTest do
     data = for value <- values,
            do: [{"created_at", value}, {"other_data", "xxx"}]
     convert_to_list_of_maps data
+  end
+
+  test "test full chain" do
+    use_cassette "github elixir-lang 3 results" do
+      result = capture_io fn ->
+        main(["elixir-lang", "elixir", "3"])
+      end
+      assert :ok == main(["elixir-lang", "elixir", "3"])
+      assert result == """
+      numb | created_at           | title                                                      
+      -----+----------------------+------------------------------------------------------------
+      3011 | 2015-01-11T17:06:06Z | Discuss if we should support monitoring callbacks in agents
+      3254 | 2015-04-07T22:12:11Z | Do not allow tuple.foo calls anymore                       
+      3268 | 2015-04-14T08:11:29Z | Warn if variable is used as function call                  
+      """
+    end
   end
 end
