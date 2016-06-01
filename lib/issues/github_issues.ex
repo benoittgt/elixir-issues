ce6b9ccb7d9e8c5f940c9cda6cbe9c19b2fc04b2
Benoit Tigeot <btigeot@octo.com>
2016-06-01 18:58:41 +0200

    Add logger info

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
+
   @user_agent [ {"User-agent", "Beni beni@beni.org"} ]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching issues for #{project} with user #{user}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -13,10 +17,12 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({ :ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
     { :ok, Poison.Parser.parse!(body) }
   end
 
-  def handle_response({ _, %{status_code: _, body: body}}) do
+  def handle_response({ _, %{status_code: status, body: body}}) do
+    Logger.error "Error with status #{status}"
     { :error, Poison.Parser.parse!(body) }
   end
 end
